845a729d3e26febdf7b09775e614b045dcd755b3
sontpa@k.s-cubism.jp <sontpa@k.s-cubism.jp>
2017-06-09 15:18:42 +0700

    temp 9/6/2017

--- index.android.js
+++ index.android.js
@@ -11,7 +11,10 @@ import {
   Text,
   Dimensions,
   TouchableOpacity,
+  TextInput,
+  Button,
   Image,
+  Alert,
   View,
 } from 'react-native';
 var {width} = Dimensions.get('window');
@@ -33,32 +36,105 @@ class LocationButton extends Component {
   }
 }
 
+class MapSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={{padding: 10}}>
+            <TextInput
+              style={{height: 40}}
+              placeholder="Type here!"
+              onChangeText={(text) => this.setState({text})}
+            />
+            {/*<Text>*/}
+              {/*{this.state.text}*/}
+            {/*</Text>*/}
+            <ButtonAddress text={this.state.text} />
+          </View>
+      </View>
+    );
+  }
+}
+
+class ButtonAddress extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {json: ''};
+  }
+  async onPressFind() {
+    try {
+      let response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.text);
+      let responseJson = await response.json();
+      // console.warn(JSON.stringify(responseJson));
+      this.setState({json: responseJson});
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    const {json} = this.state;
+    // const {text} = this.state.text;
+    return (
+      <View>
+
+        {/*<Button title="Find" onPress={(json) => { this.onPressFind(json && this.state.json) }} />*/}
+        <Button title="Find" onPress={() => this.onPressFind()} />
+        {/*<Text>*/}
+          {/*{json != '' ? console.warn(JSON.stringify(json.results)) : ''}*/}
+        {/*</Text>*/}
+        {json != '' && json.results.map((result, key) => (
+        <Text key={key}>
+          latitude: {result.geometry.location.lat}{'\n'}
+          longitude: {result.geometry.location.lng}{'\n'}
+        {/*Title: {movie.title}{'\n'}*/}
+        </Text>
+        ))}
+        {/*<Text>*/}
+          {/*{console.warn(JSON.stringify(this.state.json))}*/}
+        {/*</Text>*/}
+        {/*{json.results && json.results.address_components}*/}
+      </View>
+    );
+  }
+}
 export default class ReactMaps extends Component {
   constructor(props) {
     super(props);
     // 1 latitudeDelta => 110.57KM
     // 1 longitudeDelta => 111.32KM
     this.state = {
+      json: '',
+      text: '',
       markers: [
-        {latLng: {latitude: 10.7880448, longitude: 106.683055},
+        {latLng: {latitude: 10.7826155, longitude: 106.6937289},
           image: require('./assets/1.png'),
           photo: require('./assets/1.jpg'),
-          title: 'Kirigaya Kazuto', description: 'I know you have a crush on Kazuto-kun~'
+          title: 'Turtle Lake', description: 'Address: phường 6 District 3 Ho Chi Minh Vietnam'
         },
-        {latLng: {latitude: 10.7810449, longitude: 106.685025},
+        {latLng: {latitude: 10.7705018, longitude: 106.7107223},
           image: require('./assets/2.png'),
           photo: require('./assets/2.jpg'),
-          title: 'Yukii Asuna', description: 'You can get married!'
+          title: 'Hầm Thủ Thiêm', description: 'Address: Hầm Thủ Thiêm, Thủ Thiêm, Quận 2, Hồ Chí Minh, Vietnam'
         },
-        {latLng: {latitude: 10.7860542, longitude: 106.687015},
+        {latLng: {latitude: 10.7747954, longitude: 106.7006779},
           image: require('./assets/3.png'),
           photo: require('./assets/3.jpg'),
-          title: 'Lisbeth', description: 'Kirito-kun, Watashi wa daisuki ... =.='
+          title: 'Phố đi bộ Nguyễn Huệ', description: 'Address: Nguyễn Huệ Bến Nghé Quận 1 Hồ Chí Minh, Vietnam'
+        },
+        {latLng: {latitude: 10.7683238, longitude: 106.7044472},
+          image: require('./assets/4.png'),
+          photo: require('./assets/4.jpg'),
+          title: 'Ho Chi Minh Museum Bến Nhà Rồng', description: 'Address: 1 Nguyễn Tất Thành, phường 12, Quận 4, Hồ Chí Minh, Vietnam'
         }
       ],
       region: {
-        latitude: 10.7880448,
-        longitude: 106.683055,
+        latitude: 10.7826155,
+        longitude: 106.6937289,
         latitudeDelta: 0.0122,
         longitudeDelta: 0.0121,
       },
@@ -66,9 +142,23 @@ export default class ReactMaps extends Component {
     };
     this.onRegionChange = this.onRegionChange.bind(this);
     this.moveMaptoLocation = this.moveMaptoLocation.bind(this);
+    this.searchAddress = this.searchAddress.bind(this);
+  }
+  async getMoviesFromApi() {
+    try {
+      let response = await fetch('https://facebook.github.io/react-native/movies.json');
+      let responseJson = await response.json();
+      // console.warn(JSON.stringify(responseJson.title));
+      this.setState({json: responseJson})
+    } catch(error) {
+      console.error(error);
+    }
   }
   onRegionChange(region) {
     this.setState({region})
+  }
+  searchAddress(text) {
+    this.setState({text})
   }
   moveMaptoLocation(latLng) {
     // this.refs.map.animateToCoordinate({
@@ -85,8 +175,13 @@ export default class ReactMaps extends Component {
     //   }
     // })
 }
+  async componentDidMount () {
+    await this.getMoviesFromApi();
+  }
 
   render() {
+    const { json } = this.state;
+    // console.warn(JSON.stringify(json));
     let coordinates = this.state.markers.map(marker => marker.latLng);
     return (
       <View
@@ -162,14 +257,24 @@ export default class ReactMaps extends Component {
         </MapView>
         <View 
         style={styles.message}>
+          <MapSearch text={this.state.text} />
           <Text
             style={styles.text}>
-            latitude: <Text style={styles.colorRed}>{this.state.region.latitude}{'\n'}</Text>
-            longitude: <Text style={styles.colorRed}>{this.state.region.longitude}{'\n'}</Text>
-            latitudeDelta: <Text style={styles.colorRed}>{this.state.region.latitudeDelta}{'\n'}</Text>
-            longitudeDelta: <Text style={styles.colorRed}>{this.state.region.longitudeDelta}{'\n'}</Text>
-            <Text style={styles.colorRed}>1 latitudeDelta => 110.57 km{'\n'}</Text>
-            <Text style={styles.colorRed}>1 longitudeDelta => 111.32 km{'\n'}</Text>
+            {/*latitude: <Text style={styles.colorRed}>{this.state.region.latitude}{'\n'}</Text>*/}
+            {/*longitude: <Text style={styles.colorRed}>{this.state.region.longitude}{'\n'}</Text>*/}
+            {/*latitudeDelta: <Text style={styles.colorRed}>{this.state.region.latitudeDelta}{'\n'}</Text>*/}
+            {/*longitudeDelta: <Text style={styles.colorRed}>{this.state.region.longitudeDelta}{'\n'}</Text>*/}
+            {/*<Text style={styles.colorRed}>1 latitudeDelta => 110.57 km{'\n'}</Text>*/}
+            {/*<Text style={styles.colorRed}>1 longitudeDelta => 111.32 km{'\n'}</Text>*/}
+            {/*<Text>{this.state.json.description}</Text>*/}
+            
+            {/*{json && json.movies.map((movie, key) => (*/}
+              {/*<Text key={key}>*/}
+                {/*Key: {key}{'\n'}*/}
+                {/*Title: {movie.title}{'\n'}*/}
+                {/*releaseYear: {movie.releaseYear}{'\n'}*/}
+              {/*</Text>*/}
+            {/*))}*/}
           </Text>
         </View>
       </View>
@@ -210,17 +315,14 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     width: width,
-    height: 100,
-    backgroundColor: 'rgba(52, 52, 52, 0.5)'
+    height: 'auto',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    // zIndex: -999 //hidden =.=
   },
   pin: {
-    backgroundColor: 'rgba(51, 52, 52, 0.2)',
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: '#000',
-    borderWidth: 2,
     padding: 5,
-    borderRadius: 16,
   },
   pinImage: {
     width: 25,
